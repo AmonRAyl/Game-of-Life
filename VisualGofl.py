679b69023ae44412b658cb24c3587e71f2716e37
1566576 <raul.loscertales@e-campus.uab.cat>
2023-02-07 18:12:53 +0100

    Added Mouse Interaction And Menu Buttons

--- VisualGofl.py
+++ VisualGofl.py
@@ -3,7 +3,7 @@ import sys
 import pygame
 import copy
 
-SCREEN_WIDTH = 1001
+SCREEN_WIDTH = 1068
 SCREEN_HEIGHT = 1001
 
 
@@ -47,30 +47,30 @@ def init():
     boardSize = 50
     board = [[Cell(1+20*i, 1+20*j, white, 0)
               for i in range(boardSize)] for j in range(boardSize)]
-
-    # Glider
-    # board[10][10].setState(green)
-    # board[10][10].setValue(1)
-
-    # board[11][11].setState(green)
-    # board[11][11].setValue(1)
-
-    # board[11][12].setState(green)
-    # board[11][12].setValue(1)
-
-    # board[9][12].setState(green)
-    # board[9][12].setValue(1)
-
-    # board[10][12].setState(green)
-    # board[10][12].setValue(1)
-
-    # Random
-    for i in range(boardSize):
-        for j in range(boardSize):
-            if(random.randint(0, 1) == 1):
-                board[i][j].setState(green)
-                board[i][j].setValue(1)
-    return board, screen
+    
+    #Play Pause buttons
+    font = pygame.font.SysFont("Arial", 30)
+    
+    text = font.render("Play", 1, ( 69, 49, 7 ))
+    text2 = font.render("Pause", 1, ( 69, 49, 7 ))
+    text3 = font.render("Clear", 1, ( 69, 49, 7 ))
+    
+    x, y, w , h = text.get_rect()
+    x, y, w2 , h2 = text2.get_rect()
+    x, y, w3 , h3 = text3.get_rect()
+    
+    pygame.draw.rect(screen, (  233, 169, 36 ), (1000, 0, 68 , h))
+    playButton=screen.blit(text,  (1000, 0)) 
+    
+    pygame.draw.rect(screen, (  233, 169, 36 ), (1000, h+1, 68 , h2))
+    pauseButton=screen.blit(text2,  (1000, h)) 
+    
+    pygame.draw.rect(screen, (  233, 169, 36 ), (1000, h+h2+2, 68 , h3))
+    clearButton=screen.blit(text3,  (1000, h2+h)) 
+    
+    pygame.display.flip() 
+    
+    return board, screen,playButton,pauseButton,clearButton
 
 
 def check_cells(board):
@@ -110,28 +110,48 @@ def check_dead_or_alive(board, x, y):
         return 1, (0, 255, 0)
     return 0, (255, 255, 255)
 
-
-def main():
- # User input number of iterations
-    board, screen = init()
+def boardDisplay(board,screen): 
     for i in range(0, 50):
         for j in range(0, 50):
             pygame.draw.rect(screen, board[i][j].getState(), pygame.Rect(
                 board[i][j].getX(), board[i][j].getY(), 19, 19))
     pygame.display.flip()
+
+def main():
+ # User input number of iterations
+    board, screen,playButton,pauseButton,clearButton = init()
+    boardDisplay(board,screen)
+    
+    play=False
+    
     while True:
+        mousex, mousey = pygame.mouse.get_pos()
+        mousex=int(mousex/20)
+        mousey=int(mousey/20)
         # Posibles entradas del teclado y mouse
-        alt_board = copy.deepcopy(board)
-        board = check_cells(board)
-        for i in range(0, 50):
-            for j in range(0, 50):
-                if alt_board[i][j].getState() != board[i][j].getState():
-                    pygame.draw.rect(screen, board[i][j].getState(), pygame.Rect(
-                        board[i][j].getX(), board[i][j].getY(), 19, 19))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
-
-
+             sys.exit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if playButton.collidepoint(event.pos):
+                    play=True
+                if pauseButton.collidepoint(event.pos):
+                    play=False
+                if clearButton.collidepoint(event.pos):
+                    play=False
+                    board, screen, = init()[0:2]
+                if mousex <50 and mousex>=0 and mousey <50 and mousey>=0:
+                    if board[mousey][mousex].getValue()==1:
+                        board[mousey][mousex].setState((255, 255, 255))
+                        board[mousey][mousex].setValue(0)
+                    else:
+                        board[mousey][mousex].setState((0, 255, 0))
+                        board[mousey][mousex].setValue(1)
+                    boardDisplay(board,screen)
+        if(play==True):
+            alt_board = copy.deepcopy(board)
+            board = check_cells(board)
+            boardDisplay(board,screen)
+        pygame.display.flip()
+       
 main()
